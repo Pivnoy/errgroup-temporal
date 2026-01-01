f9258a65c30d69bf3e309f13dfdffbaa9fbe2a1c
Pivnoy <evgenij_makarev20021@mail.ru>
2026-01-01 18:45:23 +0300

    add analog of sync.Once

--- errgroup.go
+++ errgroup.go
@@ -1,28 +1,31 @@
 package errgroup_temporal
 
 import (
-	"sync"
+	"errors"
 
 	"go.temporal.io/sdk/workflow"
 )
 
 type group struct {
 	wg      workflow.WaitGroup
-	cancel  func()
-	errOnce sync.Once
+	errOnce Once
 	err     error
+
+	ctx    workflow.Context
+	cancel workflow.CancelFunc
 }
 
-func NewErrGroup(ctx workflow.Context) (*group, workflow.Context) {
+func NewErrGroup(ctx workflow.Context) *group {
 	ctx, cancel := workflow.WithCancel(ctx)
 	return &group{
+		ctx:    ctx,
 		cancel: cancel,
 		wg:     workflow.NewWaitGroup(ctx),
-	}, ctx
+	}
 }
 
-func (g *group) Wait(ctx workflow.Context) error {
-	g.wg.Wait(ctx)
+func (g *group) Wait() error {
+	g.wg.Wait(g.ctx)
 	if g.cancel != nil {
 		g.cancel()
 	}
@@ -30,18 +33,21 @@ func (g *group) Wait(ctx workflow.Context) error {
 	return g.err
 }
 
-func (g *group) Go(ctx workflow.Context, f func() error) {
+func (g *group) Go(f func(ctx workflow.Context) error) {
 	g.wg.Add(1)
-	workflow.Go(ctx, func(_ workflow.Context) {
+
+	workflow.Go(g.ctx, func(ctxFn workflow.Context) {
 		defer g.wg.Done()
 
-		if err := f(); err != nil {
-			g.errOnce.Do(func() {
+		if err := f(ctxFn); err != nil {
+			if err := g.errOnce.Do(ctxFn, func() {
 				g.err = err
 				if g.cancel != nil {
 					g.cancel()
 				}
-			})
+			}); err != nil {
+				g.err = errors.Join(g.err, err)
+			}
 		}
 	})
 }
